3b3554a4939b771bf09a9f206583af009e36a645
algorithm <odjaidri@gmail.com>
2021-06-17 19:23:54 +0100

    lesson07 - Context API (displayName)

--- src/contexts/CounterContext.jsx
+++ src/contexts/CounterContext.jsx
@@ -1,6 +1,7 @@
 import React, { createContext, useState } from "react";
 
 export const CounterCxt = createContext(0);
+CounterCxt.displayName = "Counter";
 
 const CounterContextProvider = (props) => {
   const [counter, setCounter] = useState(0);
